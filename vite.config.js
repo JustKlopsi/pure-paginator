955236a6472ba80d6e232e1b2891235a05ad2dfd
Oliver Thomas <thomas@hh-vision.de>
2023-10-23 08:28:56 +0200

    [TASK] added lib mode to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 // vite.config.js
 export default {
   // config options
-  base: '/pure-paginator/'
-};
+  base: '/pure-paginator/',
+  plugins: [cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: 'pure-paginator.js',
+      name: 'pure-paginator',
+    }
+  }
+}
